80922441b0c577a6bb108f0d76f406c1d67690df
Noah Ford <143020832+NoahFordGit@users.noreply.github.com>
2026-08-31 00:03:09 -0400

    Updated to use Sidebar and Tasklist

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,39 @@
-import ApiTester from './components/ApiTester/ApiTester';
+import './App.css';
+import Sidebar from './components/Sidebar/Sidebar';
+import TaskList from './components/Tasks/TaskList/TaskList';
+
+import type { Task } from './types/task';
+import { useState, useEffect } from 'react';
+import { getTasks } from './services/taskService';
 
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  useEffect(() => {
+    async function loadTasks() {
+      const data = await getTasks();
+      setTasks(data);
+    }
+
+    loadTasks();
+  }, [])
+
   return (
-    <div>
-      <ApiTester />
+    <div className="app-shell">
+      <Sidebar />
+
+      <main className="content-panel">
+        <header className="content-panel__header">
+          <div>
+            <p className="eyebrow">Today</p>
+            <h2>Tasks</h2>
+          </div>
+        </header>
+
+        <TaskList tasks={tasks} />
+      </main>
     </div>
-  )
+  );
 }
 
 export default App;
