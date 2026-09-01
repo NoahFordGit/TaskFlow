f3834533b84abe7bd63c04d47f6b2b29c328fa87
noahfordgit <noahmichaelford@gmail.com>
2026-09-01 19:31:44 -0400

    Created dynamic Task List Header

---
+++ frontend/src/components/Tasks/TaskListHeader/TaskListHeader.tsx
@@ -0,0 +1,11 @@
+import './TaskListHeader.css';
+
+function TaskListHeader() {
+    return (
+        <header className="task-list-header">
+            <h2 className="task-list-header__title">Your Tasks</h2>
+        </header>
+    );
+}
+
+export default TaskListHeader;
